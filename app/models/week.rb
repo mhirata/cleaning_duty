0b4d9d398f5bad6e7166b31face872593b5bf2b4
Moriyuki Hirata <m-hirata@esm.co.jp>
2011-11-17 12:56:15 +0900

    initializeの書き方間違えてた。オーバーライドは不要。

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,2 +1,12 @@
 class Week < ActiveRecord::Base
+
+  #def initialize(date)
+  #  @start_date = date
+  #end
+
+  def ==(obj)
+  p self.start_date
+  p obj.start_date
+    self.start_date == obj.start_date
+  end
 end
